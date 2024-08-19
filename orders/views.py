6337fa39ba69a6dab22f21335729813511f5addc
Grzegorz Zabaglo <grzegorz.zabago@wp.pl>
2024-08-19 17:15:02 +0200

    feat CreateOrderView check does order already exist

--- orders/views.py
+++ orders/views.py
@@ -14,40 +14,52 @@ class CreateOrderView(LoginRequiredMixin):
     @staticmethod
     def create_order(request):
         context = {}
+
         user = request.user
         customer = CustomUser.objects.get(id=user.id)
-
         address = customer.address
         postal_code = customer.postal_code
+
         cart = Cart(request)
         products = cart
         order_quantity = cart.__len__()
         total_price = cart.get_sub_total_price()
         date = datetime.today()
-        order_before_payment = Order(
-                                customer=customer,
-                                order_quantity=order_quantity,
-                                address=address,
-                                postal_code=postal_code,
-                                date=date,
-                                total_price=total_price
-                                )
-        order_before_payment.save()
-        for product in products:
-            item = Product.objects.get(id=product['product'].id)
-            ic(item)
-            order_product = ProductOrder(product=item, order=order_before_payment, product_quantity=product['product'].quantity, product_price=product['product'].price)
-            order_product.save()
 
-        context['customer'] = order_before_payment.customer
+        order_before_payment = CreateOrderView.check_order(request, customer, products, total_price)
+
+        if not order_before_payment:
+            order_before_payment = Order(
+                                    customer=customer,
+                                    order_quantity=order_quantity,
+                                    address=address,
+                                    postal_code=postal_code,
+                                    date=date,
+                                    total_price=total_price
+                                    )
+            order_before_payment.save()
+            for product in products:
+                item = Product.objects.get(id=product['product'].id)
+                order_product = ProductOrder(product=item, order=order_before_payment, quantity=product['product'].quantity, price=product['product'].price)
+                order_product.save()
+
+        context['customer'] = customer
+        context['address'] = address
+        context['postal_code'] = postal_code
         context['order_number'] = order_before_payment.id
-        context['order_quantity'] = order_before_payment.order_quantity
-        context['address'] = order_before_payment.address
-        context['postal_code'] = order_before_payment.postal_code
-        context['total_price'] = order_before_payment.total_price
+        context['order_quantity'] = order_quantity
+        context['total_price'] = total_price
         context['products'] = products
 
         return render(request, 'orders/create_order.html', context)
+
+    @staticmethod
+    def check_order(request, customer, products, total_price):
+        order = Order.objects.get(customer=customer,  total_price=total_price)
+
+        if not order:
+            return False
+        return order
 
 
 class YourOrderListView(LoginRequiredMixin, TemplateView):
@@ -70,7 +82,6 @@ class OrderDetailView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         order = Order.objects.get(id=context['pk'])
         products = ProductOrder.objects.filter(order=order)
-
 
         context['orders'] = order
         context['products'] = products
