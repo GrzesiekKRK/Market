934d3401a5bf9939230e8124fd8c42e6fff0f3af
Grzegorz Zabaglo <grzegorz.zabago@wp.pl>
2025-06-16 18:40:49 +0200

    fix admin panel search and list display

--- users/admin.py
+++ users/admin.py
@@ -4,8 +4,19 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_filter = ["role", "id", "pesel", "email"]
-    search_fields = ("role",)
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "role",
+        "is_active",
+        "date_joined",
+    ]
+    list_filter = ["role", "is_active", "date_joined"]
+    ordering = ["-date_joined"]
+    search_fields = ["email", "username", "first_name", "last_name"]
+    list_per_page = 25
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
