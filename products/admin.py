36bc8cfcc8146a4ff4bcf96d50f368da899b8859
Grzegorz Zabaglo <grzegorz.zabago@wp.pl>
2025-06-16 19:28:20 +0200

    feat admin search bar to lookup by username/names or foreignkey name

--- products/admin.py
+++ products/admin.py
@@ -6,7 +6,7 @@ from .models import Category, Product, ProductDimension, ProductImage
 class ProductAdmin(admin.ModelAdmin):
     list_filter = ["name"]
     ordering = ["-name"]
-    search_fields = ["name", "price"]
+    search_fields = ["name", "price", "category__name"]
     list_per_page = 25
 
 
@@ -19,9 +19,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductDimensionAdmin(admin.ModelAdmin):
     list_filter = ["product"]
+    search_fields = ["product__name"]
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImage)
-admin.site.register(ProductDimension)
+admin.site.register(ProductDimension, ProductDimensionAdmin)
--- wishlists/admin.py
+++ wishlists/admin.py
@@ -6,6 +6,7 @@ from .models import Wishlist
 class WishlistAdmin(admin.ModelAdmin):
     list_filter = ["user"]
     ordering = ["-user"]
+    search_fields = ["user__username", "user__first_name", "user__last_name"]
     list_per_page = 25
 
 
